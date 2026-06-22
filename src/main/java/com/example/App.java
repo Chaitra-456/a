1fb97e0f7f3f02845771271cee4b86b48aca4f36
Chaitra-456 <chaitraaj953@gmail.com>
2026-06-22 22:32:03 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+         System.out.println( "This is a real world example" );
+          System.out.println( "The sum of 5.0 and 10.0 is 15.0"  );
+    }
+}
